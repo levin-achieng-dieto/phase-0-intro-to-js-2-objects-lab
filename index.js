ee5a6b1246334d352c97306bb970cbc3b6df7820
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-08-04 08:01:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+const employee = {};
+employee.name = "levin";
+employee.streetAddress = "moiAvenue";
+
+employee;
+
+function updateEmployeeWithKeyAndValue(){
+    const employees = {...employee};
+    employees.name = "Sam"
+    employees.streetAddress = "11 Broadway"
+    return employees
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+    return employee
+}
+
+function deleteFromEmployeeByKey(){
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(){
+    delete employee.name;
+    return employee;
+}
